31e77493eed6b74ba1f87bcdae9e1b8cce9db71c
GustavoSLV <gustavos_luz@hotmail.com>
2023-08-28 14:58:21 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input()
+  PhotoCover:string = "https://www.esports.net/br/wp-content/uploads/sites/3/2023/08/loud-2-1024x683.webp"
+  @Input()
+  CardTitle:string = "EG se consagra como campeão do mundial de valorant 2023 "
   constructor() { }
 
   ngOnInit(): void {
